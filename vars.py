d305a3274a63d6e9c84f692a1901175f16adb1c1
Sheru Git <thakorjanak2003@gmail.com>
2025-09-08 22:40:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8137366352:AAHLuIWGPqilBXcec6nzHBhy88Tv
 CREDIT = os.environ.get("CREDIT", "THAKOR")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://janakthakor407_db_userTe4bpN7MbtvCvEUe@cluster0.yttziwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://thakorjanak2003_db_user:MXUAMijL5XqVCUEU@cluster0.5ilvvax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
