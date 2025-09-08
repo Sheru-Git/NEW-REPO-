3c118a52e87073218453525ac79de02b7cb9cbac
Sheru Git <thakorjanak2003@gmail.com>
2025-09-08 21:31:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ API_ID = int(os.environ.get("API_ID", "27053516")
 API_HASH = os.environ.get("API_HASH", "9a9bf031fae47c7f9a315be709bff039")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8137366352:AAHLuIWGPqilBXcec6nzHBhy88TvnrzY-S0")
 
-CREDIT = os.environ.get("CREDIT", "⌯ FʀᴏɴᴛMᴀɴ | ×͜× |")
+CREDIT = os.environ.get("CREDIT", "THAKOR")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://janakthakor407_db_userTe4bpN7MbtvCvEUe@cluster0.yttziwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
